419614f0dd5a2e70d22b98d634053905b764a3ac
Andrea <andrea.benaglia@cern.ch>
2018-04-16 11:36:52 +0200

    lxplus updates

--- rootlogon.C
+++
@@ -1,6 +0,0 @@
-{
-    gSystem->Load("CfgManager/lib/libCfgManager.so");
-    gSystem->Load("DynamicTTree/lib/libDynamicTTree.so");
-    gSystem->Load("lib/libH4Analysis.so");
-    gStyle->SetPalette(51);
-}
