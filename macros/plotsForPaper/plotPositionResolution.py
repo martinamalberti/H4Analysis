4430b753c5eff44c3274be09044740fa819fd6d9
Martina <martina.malberti@cern.ch>
2020-07-16 12:59:46 +0200

    updates

--- macros/plotsForPaper/plotPositionResolution.py
+++ macros/plotsForPaper/plotPositionResolution.py
@@ -16,12 +16,12 @@ tdrstyle.setTDRStyle()
 ROOT.gStyle.SetOptStat(0)
 ROOT.gStyle.SetOptFit(0)
 ROOT.gStyle.SetTitleOffset(1.05,'Y')
+ROOT.gStyle.SetPadRightMargin(1.2)
 ROOT.gErrorIgnoreLevel = ROOT.kWarning;
 
 Vbias = sys.argv[1]
 thr = sys.argv[2]
 angle = sys.argv[3]
-ampCorrType = 'pulseInt'
 
 filename = '../../v11/output_3bars_Vbias%s_thr%sADC_xyangle%s_runs6872-6913.root'%(Vbias, thr, angle)
 channelType = 'LYSO:Ce 3x3x57 mm^{3} - HPK S12572-015 3x3 mm^{2}'
@@ -32,19 +32,27 @@ if (angle == '4'):
     channelType = 'LYSO:Ce 3x4x57 mm^{3} - FBK NUV-HD-TE 5x5 mm^{2}'
     channel = 'BAR0'
 
+tChType = ROOT.TLatex( 0.13, 0.92,channelType)
+tChType.SetNDC()
+tChType.SetTextSize(0.030)
+
+
 f = ROOT.TFile.Open(filename)
 
-h = f.Get('h_posX_residuals_pulseIntCorr_%s'%(channel))
+h  = f.Get('h_posX_residuals_pulseIntCorr_%s'%(channel))
+#h2 = f.Get('h2_posX_residuals_pulseIntCorr_vs_posX_%s'%(channel))
+h2 = f.Get('h2_posX_residuals_ampCorr_vs_posX_%s'%(channel))
+h2_pfx = h2.ProfileX('')
+
 
 canvas = ROOT.TCanvas('position_residuals_%s'%(channel),'position_residuals_%s'%(channel))
-h.GetXaxis().SetRangeUser(-40,40)
+h.GetXaxis().SetRangeUser(-30,30)
 h.GetYaxis().SetRangeUser(0,h.GetMaximum()*1.1)
-h.GetXaxis().SetTitle('x_{reco} -x_{track} (mm)')
+h.GetXaxis().SetTitle('x_{reco} - x_{track} (mm)')
 h.GetYaxis().SetTitle('entries')
 h.Draw('e')
 h.Fit('gaus','Q','', h.GetMean()-3*h.GetRMS(),h.GetMean()+3*h.GetRMS())
-canvas.Modified()
-canvas.Update()
+tChType.Draw()
 
 text = '#sigma_{x} = %.2f #pm %.2f mm'%(h.GetFunction('gaus').GetParameter(2), h.GetFunction('gaus').GetParError(2))
 latex = ROOT.TLatex( 0.15, 0.82, text )
@@ -52,25 +60,24 @@ latex.SetNDC()
 latex.SetTextSize(0.035)
 latex.Draw()
 
-#ROOT.gStyle.SetOptFit(0)
-#ROOT.gStyle.SetFitFormat('4.2g')
-#stat = h.GetListOfFunctions().FindObject('stats')
-#stat.SetX1NDC(0.65)
-#stat.SetX2NDC(0.90)
-#stat.SetY1NDC(0.72)
-#stat.SetY2NDC(0.88)
-#canvas.Modified()
-#canvas.Update()
-
-
-
-tChType = ROOT.TLatex( 0.13, 0.92,channelType)
-tChType.SetNDC()
-tChType.SetTextSize(0.030)
+canvas2 = ROOT.TCanvas('position_residuals_vs_x_%s'%(channel),'position_residuals_vs_x_%s'%(channel))
+binmin = h2_pfx.FindFirstBinAbove(0)
+binmax = h2_pfx.FindLastBinAbove(0)
+xmin = h2_pfx.GetBinCenter(binmin)
+xmax = h2_pfx.GetBinCenter(binmax)
+print binmin, binmax, xmin, xmax
+h2.GetXaxis().SetRangeUser(xmin,xmax)
+h2.GetYaxis().SetRangeUser(-30,30)
+h2.GetXaxis().SetTitle('x_{track} (mm)')
+h2.GetYaxis().SetTitle('x_{reco} - x_{track} (mm)')
+h2.Draw('colz')
+h2_pfx = h2.ProfileX('')
+h2_pfx.Draw('same')
 tChType.Draw()
 
-canvas.SaveAs(canvas.GetName()+'.png')
-canvas.SaveAs(canvas.GetName()+'.pdf')
-canvas.SaveAs(canvas.GetName()+'.C')
+for c in canvas, canvas2:
+    c.SaveAs(c.GetName()+'.png')
+    c.SaveAs(c.GetName()+'.pdf')
+    c.SaveAs(c.GetName()+'.C')
 
 #raw_input('ok?')
--- macros/plotsForPaper/plotTimeResolutionGlobalLocal.py
+++ macros/plotsForPaper/plotTimeResolutionGlobalLocal.py
@@ -22,6 +22,7 @@ Vbias = sys.argv[1]
 thr = sys.argv[2]
 angle = sys.argv[3]
 ampCorrType = 'pulseInt'
+useTdiff = True
 
 filename = '../../v11/output_3bars_Vbias%s_thr%sADC_xyangle%s_runs6872-6913.root'%(Vbias, thr, angle)
 channelType = 'LYSO:Ce 3x3x57 mm^{3} - HPK S12572-015 3x3 mm^{2} '
@@ -42,6 +43,9 @@ colors = {'global' : ROOT.kBlack,
 h0 = {}
 h0['global'] = f.Get('h_tAve_%sCorr_%s'%(ampCorrType,channel))            
 h0['local']  = f.Get('xBins/h_tAve_%sCorr_binX_%d_%s'%(ampCorrType,xbin,channel))
+if (useTdiff):
+    h0['global'] = f.Get('h_tDiff_%sCorr_posCorr_%s'%(ampCorrType,channel))            
+    h0['local']  = f.Get('xBins/h_tDiff_%sCorr_posCorr_binX_%d_%s'%(ampCorrType,xbin,channel))
 nRe = int(h0['global'].GetNbinsX()/h0['local'].GetNbinsX())
 h0['global'].Rebin(nRe)
 
@@ -50,6 +54,9 @@ h = {}
 nbins = h0['global'].GetNbinsX()
 xmin = h0['global'].GetXaxis().GetXmin() - h0['global'].GetMean()
 xmax = h0['global'].GetXaxis().GetXmax()- h0['global'].GetMean()
+if (useTdiff):
+    xmin = xmin/2
+    xmax = xmax/2
 h['global'] = ROOT.TH1F('h_tAve_%sCorr_global_%s'%(ampCorrType,channel),'', nbins, xmin, xmax )
 h['local'] = ROOT.TH1F('h_tAve_%sCorr_local_%s'%(ampCorrType,channel), '', nbins, xmin, xmax  )
 for ibin in range(1, nbins):
@@ -57,8 +64,7 @@ for ibin in range(1, nbins):
     h['global'].SetBinError(ibin, h0['global'].GetBinError(ibin))
     h['local'].SetBinContent(ibin, h0['local'].GetBinContent(ibin))
     h['local'].SetBinError(ibin, h0['local'].GetBinError(ibin))
-
-
+    
 fGaus =  {}
 for i, label in enumerate(labels):
     h[label].Scale(1./h[label].Integral())
@@ -82,15 +88,19 @@ legend.AddEntry(h['global'],'w/o beam spot selection','PL')
 legend.AddEntry(h['local'],'#Deltax < 2 mm','PL')
     
 canvas = ROOT.TCanvas('tRes_global_local__%s'%channel,'tRes_global_local__%s'%channel)
-#xmin = h['global'].GetMean()-8*fGaus['global'].GetParameter(2)
-#xmax = h['global'].GetMean()+8*fGaus['global'].GetParameter(2)
 xmin = -0.25
 xmax = 0.25
 ymax = 0.50
 if (angle == '4'): ymax = 0.30
+if (useTdiff):
+    ymax = 0.3
+    if (angle == '4'): ymax = 0.20
+    
 hdummy = ROOT.TH2F('hdummy','', 100, xmin, xmax, 100, 0, ymax)
 hdummy.GetYaxis().SetTitle('a.u.')
 hdummy.GetXaxis().SetTitle('t_{average} - t_{MPC} (ns)')
+if (useTdiff):
+    hdummy.GetXaxis().SetTitle('t_{diff}/2 (ns)')
 hdummy.GetXaxis().SetNdivisions(505)
 hdummy.GetYaxis().SetTitleOffset(1.15)
 hdummy.Draw()
@@ -102,6 +112,7 @@ legend.Draw()
 tRes = {}
 text = ''
 resolMCP = 12.3
+if (useTdiff): resolMCP = 0
 for i,label in enumerate(labels):
     resolGaus = h[label].GetFunction('fGaus_%s'%label).GetParameter(2)*1000.
     resolGausErr = h[label].GetFunction('fGaus_%s'%label).GetParError(2)*1000.
@@ -122,8 +133,13 @@ tChType.SetNDC()
 tChType.SetTextSize(0.030)
 tChType.Draw()
 
-canvas.SaveAs(canvas.GetName()+'.png')
-canvas.SaveAs(canvas.GetName()+'.pdf')
-canvas.SaveAs(canvas.GetName()+'.C')
-
-raw_input('ok?')
+if (useTdiff==False):
+    canvas.SaveAs(canvas.GetName()+'.png')
+    canvas.SaveAs(canvas.GetName()+'.pdf')
+    canvas.SaveAs(canvas.GetName()+'.C')
+else:
+    canvas.SaveAs(canvas.GetName()+'_tDiff.png')
+    canvas.SaveAs(canvas.GetName()+'_tDiff.pdf')
+    canvas.SaveAs(canvas.GetName()+'_tDiff.C')
+    
+#raw_input('ok?')
